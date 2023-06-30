0d53f45176f1a234eaa9dd142b7c7681418535af
Ryan Milligan <ryanmilligan88@gmail.com>
2023-06-30 16:19:01 -0700

    fix: re-order eslint config plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,11 +15,12 @@ module.exports = {
             },
         },
     },
+    plugins: ['@typescript-eslint'],
     parser: '@typescript-eslint/parser',
     parserOptions: {
         project: './tsconfig.json',
     },
-    plugins: ['@typescript-eslint'],
+
     env: {
         browser: true,
         node: true,

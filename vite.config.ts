e7c831d97266b374167c059a932960d3fde35650
Matthew Hawksby <matthewhawksby91@gmail.com>
2023-06-30 17:48:27 -0700

    fix: vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig, loadEnv } from 'vite';
 import vercel from 'vite-plugin-vercel';
+import ssr from 'vite-plugin-ssr/plugin';
+import vercelSsr from '@magne4000/vite-plugin-vercel-ssr';
+export default defineConfig(async ({ command, mode }) => {
+  return {
+  root: './src/',
+  build: {
+    outDir: './dist/',
+  },
+  vercel: {
+    // Tweak what you need, check TS definition for details
+  },
+  plugins: [ssr(), vercel(), vercelSsr()],
+}});
 
-export default defineConfig({
-    root: './src/',
-    build: {
-        outDir: './dist/',
-    },
-    plugins: [vercel()],
-});
